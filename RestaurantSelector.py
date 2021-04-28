f48909e9a16bd8c18641c9796aab5a07182dd14b
afaalevao <andrew.faalevao@gmail.com>
2021-04-27 20:27:20 -0600

    Name, Rating, and Pricing implemented

--- RestaurantSelector.py
+++ RestaurantSelector.py
@@ -5,9 +5,41 @@ Python Program to select random restaurant given a location
 '''
 
 
-from geopy.geocoders import Nominatim
 import bs4
 import urllib.request as request
+
+
+# class to store Restaurant information
+class Restaurant:
+    def __init__(self, name, rating, price):
+        self.name = name
+        self.rating = rating
+        self.price = price
+
+
+def print_restaurants(restaurants):
+    # iterate through each restaurant and print information
+    for restaurant in restaurants:
+        try:
+            name = restaurant.find("a").text
+            print(name)
+        except:
+            print("No name found.")
+
+        try:
+            rating = restaurant.find("div", {"class": "attribute__09f24__3znwq display--inline-block__09f24__3L1EB margin-r1__09f24__BCulR border-color--default__09f24__1eOdn"}).div.get("aria-label")
+            print(rating)
+        except:
+            print("No rating found")
+
+        try:
+            price = restaurant.find("span", {"class": "priceRange__09f24__2O6le css-xtpg8e"}).text
+            print(price)
+        except:
+            print("No pricing available")
+
+        # newline for spacing
+        print("\n")
 
 
 def main():
@@ -16,13 +48,13 @@ def main():
     # get input from user
     city = input("Enter the city where you would like to eat: ")
     option = input("Would you like to eat a specific type of food? (y/n): ")
-    type = ""
+    food_type = ""
 
     if option == "y":
-        type = input("What type of food do you want to eat? (Japanese, Mexican, etc.): ")
+        food_type = input("What type of food do you want to eat? (Japanese, Mexican, etc.): ")
 
-    url = "https://www.yelp.com/search?find_desc=" + type + "&find_loc=" + city
-    print(url)
+    url = "https://www.yelp.com/search?find_desc=" + food_type + "&find_loc=" + city
+    print("\n")
 
     web_request = request.urlopen(url)
     page = bs4.BeautifulSoup(web_request, 'html.parser')
@@ -30,12 +62,7 @@ def main():
     # this line will need to be updated as yelp updates webpage
     restaurants = page.find_all("div", {"class": "mainAttributes__09f24__26-vh arrange-unit__09f24__3IxLD arrange-unit-fill__09f24__1v_h4 border-color--default__09f24__1eOdn"})
 
-    for restaurant in restaurants:
-        try:
-            name = restaurant.find("a").text
-            print(name)
-        except:
-            print("No information found.")
+    print_restaurants(restaurants)
 
 
 if __name__ == '__main__':
